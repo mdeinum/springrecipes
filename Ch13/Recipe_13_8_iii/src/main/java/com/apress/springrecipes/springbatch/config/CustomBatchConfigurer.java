006aafb3c59d8d632999d212ac2f03a79d4a7dfe
Marten Deinum <mdeinum@gmail.com>
2014-03-17 19:55:51 +0100

    Added remaining chapter 13 recipes.

---
+++ Ch13/Recipe_13_8_iii/src/main/java/com/apress/springrecipes/springbatch/config/CustomBatchConfigurer.java
@@ -0,0 +1,80 @@
+package com.apress.springrecipes.springbatch.config;
+
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+/**
+ * Custom {@code BatchConfigurer} implementation which registers a {@code SimpleAsyncTaskExecutor}.
+ *
+ * @author Marten Deinum
+ */
+@Component
+public class CustomBatchConfigurer implements BatchConfigurer {
+
+    private DataSource dataSource;
+    private PlatformTransactionManager transactionManager;
+    private JobRepository jobRepository;
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.transactionManager = new DataSourceTransactionManager(dataSource);
+    }
+
+    protected CustomBatchConfigurer() {}
+
+    @Override
+    public JobRepository getJobRepository() {
+        return jobRepository;
+    }
+
+    @Override
+    public PlatformTransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    @Override
+    public JobLauncher getJobLauncher() {
+        return jobLauncher;
+    }
+
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+    @PostConstruct
+    public void initialize() throws Exception {
+        this.jobRepository = createJobRepository();
+        this.jobLauncher = createJobLauncher();
+    }
+
+    protected JobLauncher createJobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(taskExecutor());
+        return jobLauncher;
+    }
+
+    protected JobRepository createJobRepository() throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setTransactionManager(transactionManager);
+        factory.afterPropertiesSet();
+        return  (JobRepository) factory.getObject();
+    }
+
+}
