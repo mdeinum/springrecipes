7a6248747f21df4150073bca8093ddd6eb1f221d
Marten Deinum <mdeinum@gmail.com>
2014-04-28 09:11:39 +0200

    Added recipes for chapter 17.

--- Ch17/Recipe_17_6_ii/src/main/java/com/apress/springrecipes/springintegration/ServerMain.java
+++ Ch17/Recipe_17_6_ii/src/main/java/com/apress/springrecipes/springintegration/ServerMain.java
@@ -7,18 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class Main {
+public class ServerMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("integration-context.xml");
-
-        JmsTemplate jmsTemplate = applicationContext.getBean(JmsTemplate.class);
-
-        Map<String, Object> customer = new HashMap<>();
-        customer.put("id", 1234L);
-        customer.put("firstName", "Marten");
-        customer.put("lastName", "Deinum");
-
-        jmsTemplate.convertAndSend("solution012", customer);
 
     }
 }
