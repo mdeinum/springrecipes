0b5d8fd291f43ee64246ea9ca5ed10ddc3ae0e78
Marten Deinum <mdeinum@gmail.com>
2014-05-19 11:41:58 +0200

    Added recipe 1.6. Updated code in other recipes

---
+++ Ch1/Recipe_1_6/src/main/java/com/apress/springrecipes/hello/Main.java
@@ -0,0 +1,15 @@
+package com.apress.springrecipes.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = 
+            new GenericXmlApplicationContext("beans.xml");
+
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        helloWorld.hello();
+    }
+}
--- Ch1/springintro/src/main/java/com/apress/springrecipes/hello/Main.java
+++ Ch1/springintro/src/main/java/com/apress/springrecipes/hello/Main.java
@@ -6,10 +6,9 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = 
-            new GenericXmlApplicationContext("beans.xml");
+        ApplicationContext context = new GenericXmlApplicationContext("beans.xml");
 
-        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld helloWorld = context.getBean(HelloWorld.class);
         helloWorld.hello();
     }
 }
