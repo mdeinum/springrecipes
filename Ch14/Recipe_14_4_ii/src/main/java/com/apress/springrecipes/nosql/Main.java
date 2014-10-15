75aeb11cc632382cbc53f4e7dd864b238484eb61
Marten Deinum <mdeinum@gmail.com>
2014-10-15 20:29:53 +0200

    Chapter 14

--- Ch14/Recipe_14_4_ii/src/main/java/com/apress/springrecipes/nosql/Main.java
+++ Ch14/Recipe_14_4_ii/src/main/java/com/apress/springrecipes/nosql/Main.java
@@ -1,14 +1,20 @@
 package com.apress.springrecipes.nosql;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import java.util.Map;
+
+import static com.apress.springrecipes.nosql.Main.RelationshipTypes.*;
 
 /**
  * Created by marten on 03-10-14.
  */
 public class Main {
+
+    enum RelationshipTypes implements RelationshipType {FRIENDS_WITH, MASTER_OF, SIBLING, LOCATION};
 
     public static void main(String[] args) {
         final String DB_PATH = System.getProperty("user.home") + "/friends";
@@ -16,19 +22,55 @@ public class Main {
 
         Transaction tx1 = db.beginTx();
 
-        Node hello = db.createNode();
-        hello.setProperty("msg", "Hello");
+        Label character = DynamicLabel.label("character");
+        Label planet = DynamicLabel.label("planet");
 
-        Node world = db.createNode();
-        world.setProperty("msg", "World");
+
+        // Planets
+        Node dagobah = db.createNode(planet);
+        dagobah.setProperty("name", "Dagobah");
+
+        Node tatooine = db.createNode(planet);
+        tatooine.setProperty("name", "Tatooine");
+
+        Node alderaan = db.createNode(planet);
+        alderaan.setProperty("name", "Alderaan");
+
+        // Characters
+        Node yoda = db.createNode(character);
+        yoda.setProperty("name", "Yoda");
+
+        Node luke = db.createNode(character);
+        luke.setProperty("name", "Luke Skywalker");
+
+        Node leia = db.createNode(character);
+        leia.setProperty("name", "Leia Organa");
+
+        Node han = db.createNode(character);
+        han.setProperty("name", "Han Solo");
+
+        // Relations
+        yoda.createRelationshipTo(luke, MASTER_OF);
+        yoda.createRelationshipTo(dagobah, LOCATION);
+        luke.createRelationshipTo(leia, SIBLING);
+        luke.createRelationshipTo(tatooine, LOCATION);
+        luke.createRelationshipTo(han, FRIENDS_WITH);
+        leia.createRelationshipTo(han, FRIENDS_WITH);
+        leia.createRelationshipTo(alderaan, LOCATION);
+
         tx1.success();
 
-
-        Iterable<Node> nodes = db.getAllNodes();
-        for (Node n : nodes) {
-            System.out.println("Msg: " + n.getProperty("msg"));
+        ExecutionEngine engine = new ExecutionEngine(db);
+        ExecutionResult result = engine.execute("MATCH (n) RETURN n.name as name");
+        String rows = "";
+        for ( Map<String, Object> row : result ) {
+            for ( Map.Entry<String, Object> column : row.entrySet() ) {
+                rows += column.getKey() + ": " + column.getValue() + "; ";
+            }
+            rows += "\n";
         }
+        System.out.println(rows);
 
-        // Remove all nodes
+
     }
 }
