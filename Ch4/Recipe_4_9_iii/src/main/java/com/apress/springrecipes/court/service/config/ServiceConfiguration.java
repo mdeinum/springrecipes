9533c6fe777abed37552fafa976283d8b768eb83
Marten Deinum <mdeinum@gmail.com>
2014-11-03 20:35:11 +0100

    Code  fixes.

--- Ch4/Recipe_4_9_iii/src/main/java/com/apress/springrecipes/court/service/config/ServiceConfiguration.java
+++ Ch4/Recipe_4_9_iii/src/main/java/com/apress/springrecipes/court/service/config/ServiceConfiguration.java
@@ -1,11 +1,9 @@
 package com.apress.springrecipes.court.service.config;
 
-import com.apress.springrecipes.court.domain.ReservationValidator;
 import com.apress.springrecipes.court.service.ReservationService;
 import com.apress.springrecipes.court.service.ReservationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.Validator;
 
 /**
  * Created by marten on 28-04-14.
@@ -18,8 +16,4 @@ public class ServiceConfiguration {
         return new ReservationServiceImpl();
     }
 
-    @Bean
-    public Validator reservationValidator() {
-        return new ReservationValidator();
-    }
 }
